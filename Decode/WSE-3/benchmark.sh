e99ac0eb4d77980672ccacb95015b0de3ce6187d
Junesoo Kang <junesook@tinkercliffs1.cm.cluster>
2025-10-28 12:45:42 -0400

    Initial Commit

---
+++ Decode/WSE-3/benchmark.sh
@@ -0,0 +1,19 @@
+# ./run_wse3.sh model_config/llama8B_4k_1_240.json false | tee -a log/llama8B_4k_1_240.log
+# ./run_wse3.sh model_config/llama8B_4k_1_256.json false | tee -a log/llama8B_4k_1_256.log
+# ./run_wse3.sh model_config/llama8B_4k_1_300.json false | tee -a log/llama8B_4k_1_300.log
+# ./run_wse3.sh model_config/llama8B_4k_1_360.json false | tee -a log/llama8B_4k_1_360.log
+# ./run_wse3.sh model_config/llama8B_4k_1_420.json false | tee -a log/llama8B_4k_1_420.log
+# ./run_wse3.sh model_config/llama8B_4k_1_480.json false | tee -a log/llama8B_4k_1_480.log
+# ./run_wse3.sh model_config/llama8B_4k_1_512.json false | tee -a log/llama8B_4k_1_512.log
+# ./run_wse3.sh model_config/llama8B_4k_1_540.json false | tee -a log/llama8B_4k_1_540.log
+# ./run_wse3.sh model_config/llama8B_4k_1_600.json false | tee -a log/llama8B_4k_1_600.log
+# ./run_wse3.sh model_config/llama8B_4k_1_660.json false | tee -a log/llama8B_4k_1_660.log
+# ./run_wse3.sh model_config/llama8B_4k_1_720.json false | tee -a log/llama8B_4k_1_720.log
+
+./run_wse3.sh model_config/qwen72B_4k_1_360.json false | tee -a log/qwen72B_4k_1_360.log
+./run_wse3.sh model_config/qwen72B_4k_1_420.json false | tee -a log/qwen72B_4k_1_420.log
+./run_wse3.sh model_config/qwen72B_4k_1_480.json false | tee -a log/qwen72B_4k_1_480.log
+./run_wse3.sh model_config/qwen72B_4k_1_540.json false | tee -a log/qwen72B_4k_1_540.log
+./run_wse3.sh model_config/qwen72B_4k_1_600.json false | tee -a log/qwen72B_4k_1_600.log
+./run_wse3.sh model_config/qwen72B_4k_1_660.json false | tee -a log/qwen72B_4k_1_660.log
+./run_wse3.sh model_config/qwen72B_4k_1_720.json false | tee -a log/qwen72B_4k_1_720.log
